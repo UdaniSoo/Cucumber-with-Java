766a03cc254b0e2b9f7c8e4964839baa5cf4a3f5
Udani <isuru.udani1@gmail.com>
2019-12-22 11:12:20 +0800

    TestRunner settup

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -1,5 +1,17 @@
 package cucumberOptions;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features= "/Users/Udani/eclipse-workspace/AutomationCucumber/src/test/java/features",
+		glue= "stepDefinition"
+		
+		)
+
 public class TestRunner {
 
 }
--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -9,9 +10,12 @@ public class stepDefinition {
 	
 	@Given("^User is on netbanking landing page$")
 	
-	public void user_is_on_netbanking_landing_page()
+	public void user_is_on_netbanking_landing_page() throws Throwable
 	{
 		//code to navigate to net banking landing page
+		System.out.println("User is on netbanking landing page");
+		
+		throw new PendingException();
 	
 	}
 	
@@ -20,6 +24,7 @@ public class stepDefinition {
 	public void User_login_with_Username_and_Password()
 	{
 		//Code to enter user name and password
+		System.out.println("User login to the application with Username and Password");
 	}
 	
 	@Then("^Home page is populated$")
@@ -27,13 +32,15 @@ public class stepDefinition {
 	public void Home_page_is_populated()
 	{
 		//Home page validation code
+		System.out.println("Home page is validated");
 	}	
 	
 	@And("^Cards are displayed$")
 	
 	public void Cards_are_displayed()
 	{
-		 //Cards validation code.
+		 //Cards validation code. 
+		System.out.println("Cards are validated");
 	}
 	
 
